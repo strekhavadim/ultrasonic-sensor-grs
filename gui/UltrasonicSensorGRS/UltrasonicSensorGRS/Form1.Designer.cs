75a5f4119e4cd5fd88fd60fdead67cdf5c7e4c90
Vadim Strekha <feero5525@gmail.com>
2017-12-02 12:05:21 +0200

    c# gui added

---
+++ gui/UltrasonicSensorGRS/UltrasonicSensorGRS/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace UltrasonicSensorGRS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 800);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(28)))), ((int)(((byte)(37)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(250, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 800);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.panel3.Controls.Add(this.chart1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(255, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(769, 800);
+            this.panel3.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "X1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "X2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "X3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(739, 461);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 800);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ gui/UltrasonicSensorGRS/UltrasonicSensorGRS/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltrasonicSensorGRS
+{
+    public partial class Form1 : Form
+    {
+
+        private FixedSizedQueue<SerialInputRecord> dataQueue = new FixedSizedQueue<SerialInputRecord>(10);
+        private FixedSizedQueue<SerialInputRecord> chartValues = new FixedSizedQueue<SerialInputRecord>(50);
+
+        private struct SerialInputRecord {
+            public int x1, x2, x3, y1;
+        }; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string line = serialPort1.ReadExisting();
+            if (line.Length != 0)
+            {
+                SerialInputRecord record = parseSerialInputValues(line);
+                dataQueue.Enqueue(record);
+                chartValues.Enqueue(record);
+                ProceedData(dataQueue);
+                RedrawChart(chartValues);
+            }
+        }
+
+        private SerialInputRecord parseSerialInputValues(String str)
+        {
+            SerialInputRecord obj;
+            obj.x1 = 0;
+            obj.x2 = 0;
+            obj.x3 = 0;
+            obj.y1 = 0;
+            String[] strVals = str.Split(' ');
+            int valsCount = strVals.Length;
+            int xI;
+
+            if (valsCount < 1)
+                return obj;
+            
+            if (Int32.TryParse(strVals[0], out xI))
+            {
+                obj.x1 = xI;
+            }
+
+            if (valsCount < 2)
+                return obj;
+
+            if (Int32.TryParse(strVals[1], out xI))
+            {
+                obj.x2 = xI;
+            }
+
+            if (valsCount < 3)
+                return obj;
+
+            if (Int32.TryParse(strVals[2], out xI))
+            {
+                obj.x3 = xI;
+            }
+
+            // Continue...
+
+            return obj;
+        }
+
+        private int ProceedData(FixedSizedQueue<SerialInputRecord> queue)
+        {
+            Console.WriteLine("<-----------------START----------------->");
+            PassQueue(queue, ConsoleWriteRecordData);
+            Console.WriteLine("<-----------------DONE----------------->");
+           
+            return 0;
+        }
+
+        private void ConsoleWriteRecordData(SerialInputRecord record, int i)
+        {
+            Console.WriteLine("Item: {X1:" + record.x1.ToString() + ", X2: " + record.x2.ToString() + ", X3: " + record.x3.ToString() + "}");
+        }
+
+        private void PassQueue(FixedSizedQueue<SerialInputRecord> queue, Action<SerialInputRecord,int> callback)
+        {
+            int l = queue.Size;
+            for (int i = 0; i < l; i++)
+            {
+                SerialInputRecord a;
+                queue.TryDequeue(out a);
+                queue.Enqueue(a);
+                callback(a, i);
+            }
+        }
+
+        private void RedrawChart(FixedSizedQueue<SerialInputRecord> chartQueue)
+        {
+            chart1.Series["X1"].Points.Clear();
+            chart1.Series["X2"].Points.Clear();
+            chart1.Series["X3"].Points.Clear();
+            PassQueue(chartQueue, AddChartPoints);
+        }
+
+        private void AddChartPoints(SerialInputRecord record, int i)
+        {
+            chart1.Series["X1"].Points.AddXY(i, record.x1);
+            chart1.Series["X2"].Points.AddXY(i, record.x2);
+            chart1.Series["X3"].Points.AddXY(i, record.x3);
+        }
+    }
+}
